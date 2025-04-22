08b8d5d2b5896418d64564d18472ec9da8a3d962
Luisa Martinez <lmart276@its.jnj.com>
2025-04-22 14:41:41 +0200

    Exclude "AS" from query

--- inst/sql/sql_server/GetFrequentConcepts.sql
+++ inst/sql/sql_server/GetFrequentConcepts.sql
@@ -6,15 +6,15 @@
 {DEFAULT @min_freq = 5}
 
 SELECT a.concept_name, b.concept_id, b.pop, b.pop_perc  
-FROM (SELECT  @domain_concept_id  AS concept_id, 
-      COUNT(DISTINCT h.person_id) AS pop, 
+FROM (SELECT  @domain_concept_id concept_id, 
+      COUNT(DISTINCT h.person_id) pop, 
       100*(CONVERT(numeric, COUNT(DISTINCT h.person_id))/ 
              (SELECT COUNT(DISTINCT subject_id) FROM  
               @cohort_database_schema  WHERE cohort_definition_id = 
-                @cohort_id)) AS pop_perc 
-      FROM @cdm_database_schema.@domain_table  AS h 
+                @cohort_id)) pop_perc 
+      FROM @cdm_database_schema.@domain_table h 
       WHERE person_id IN (SELECT subject_id FROM @cohort_database_schema 
                           WHERE cohort_definition_id =  @cohort_id) 
-      GROUP BY concept_id ORDER BY pop DESC) AS b 
-JOIN  @cdm_database_schema.concept AS a ON b.concept_id = a.concept_id 
-WHERE pop_perc >  @min_freq  ORDER BY pop_perc DESC;
+      GROUP BY concept_id ORDER BY pop DESC) b 
+JOIN  @cdm_database_schema.concept a ON b.concept_id = a.concept_id 
+WHERE pop_perc >  @min_freq ORDER BY pop_perc DESC;
